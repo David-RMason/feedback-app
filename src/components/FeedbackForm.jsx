a3c6926b5986efdb4f4bf655d281342d16c88874
David Mason <masondr@live.co.uk>
2026-03-27 12:21:07 +0000

    feat: Create feedback form

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import Card from "./shared/Card";
+
+export default function FeedbackForm() {
+  const [text, setText] = useState("");
+
+  function handleTextChange(e) {
+    setText(e.target.value);
+  }
+
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate your service with us?</h2>
+        <div className="input-group">
+          <input
+            type="text"
+            placeholder="Write a review"
+            onChange={handleTextChange}
+            value={text}
+          ></input>
+          <button type="submit">Send</button>
+        </div>
+      </form>
+    </Card>
+  );
+}
